545ed34f77c727791e7150d3af452f01fb75fe56
Nick Dowmon <nick.dowmon@jupiterone.com>
2021-07-22 17:58:28 -0400

    Add message form

--- js/index.js
+++ js/index.js
@@ -19,3 +19,35 @@ for (const skillString of skills) {
   skill.innerHTML = skillString;
   skillsList.appendChild(skill);
 }
+
+//-----------------------------------------
+
+const messageForm = document.querySelector('form');
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = e.target.name.value;
+  const email = e.target.email.value;
+  const message = e.target.message.value;
+
+  console.log({
+    name,
+    email,
+    message,
+  });
+
+  const messagesSection = document.getElementById('messages');
+  const messagesList = messagesSection.querySelector('ul');
+  const newMessage = document.createElement('li');
+  newMessage.innerHTML = `<a href=mailto:${email}>${name}</a> wrote: <span>${message} </span>`;
+  const removeButton = document.createElement('button');
+  removeButton.innerHTML = 'remove';
+  removeButton.type = 'button';
+  removeButton.addEventListener('click', (e) => {
+    const entry = e.target.parentNode;
+    entry.remove();
+  });
+  newMessage.appendChild(removeButton);
+  messagesList.appendChild(newMessage);
+  messageForm.reset();
+})
